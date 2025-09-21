3766ea98adc97dcfc2d150127acf8fa7029b2849
Norhan Hassan <nwrhanhsnly@gmail.com>
2025-09-21 17:28:29 +0300

    lab 2 added

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,4 +1,8 @@
-import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import {
+  NgModule,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection,
+} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
@@ -7,23 +11,11 @@ import { Header } from './components/header/header';
 import { Footer } from './components/footer/footer';
 import { SideMenu } from './components/side-menu/side-menu';
 import { Products } from './components/products/products';
-
+import { FormsModule } from '@angular/forms';
 @NgModule({
-  declarations: [
-    App,
-    Header,
-    Footer,
-    SideMenu,
-    Products
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection()
-  ],
-  bootstrap: [App]
+  declarations: [App, Header, Footer, SideMenu, Products],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+  providers: [provideBrowserGlobalErrorListeners(), provideZonelessChangeDetection()],
+  bootstrap: [App],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/components/products/products.ts
+++ src/app/components/products/products.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Store } from '../../models/store';
+import { ICategory } from '../../models/icategory';
+import { IProduct } from '../../models/iproduct';
 
 @Component({
   selector: 'app-products',
@@ -10,8 +12,83 @@ import { Store } from '../../models/store';
 export class Products {
   store: Store;
   clientName = 'Norhan';
+  searchTerm: string = '';
+  selectedCategory: number = 0;
 
   constructor() {
     this.store = new Store('store1', ['Cairo Branch', 'Alex Branch'], 'assets/store.jpeg');
+  }
+  categories: ICategory[] = [
+    { id: 1, name: 'Clothes' },
+    { id: 2, name: 'Accessories' },
+    { id: 3, name: 'Shoes' },
+  ];
+
+  products: IProduct[] = [
+    {
+      id: 1,
+      name: 'T-Shirt',
+      quantity: 5,
+      price: 200,
+      img: 'https://placehold.co/600x400',
+      categoryID: 1,
+    },
+    {
+      id: 2,
+      name: 'Jeans',
+      quantity: 2,
+      price: 400,
+      img: 'https://placehold.co/600x400',
+      categoryID: 1,
+    },
+    {
+      id: 3,
+      name: 'Necklace',
+      quantity: 0,
+      price: 150,
+      img: 'https://placehold.co/600x400',
+      categoryID: 2,
+    },
+    {
+      id: 4,
+      name: 'Sneakers',
+      quantity: 10,
+      price: 600,
+      img: 'https://placehold.co/600x400',
+      categoryID: 3,
+    },
+  ];
+
+  buy(product: IProduct) {
+    if (product.quantity >= 2) {
+      product.quantity -= 2;
+    }
+  }
+
+  showDetails(product: IProduct) {
+    alert(`
+      Product: ${product.name}
+      Price: ${product.price}
+      Quantity: ${product.quantity}
+    `);
+  }
+  get filteredProducts() {
+    return this.products.filter(
+      (p) =>
+        p.name.toLowerCase().includes(this.searchTerm.toLowerCase()) &&
+        (this.selectedCategory == 0 || p.categoryID == this.selectedCategory)
+    );
+  }
+  getQuantityStatus(qty: number): string {
+    switch (qty) {
+      case 0:
+        return 'Out of stock';
+      case 1:
+        return 'Last one item';
+      case 2:
+        return 'Last two items';
+      default:
+        return 'In stock';
+    }
   }
 }
